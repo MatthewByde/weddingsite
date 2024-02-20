759007b16e6a697955fb32c16ef9990b3832cf0d
mdb978 <mdb978@student.bham.ac.uk>
2024-02-20 22:47:28 +0000

    fix: static path needed to be relative to build folder, not to server folder

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,7 +3,7 @@ import express from 'express';
 export default function main() {
     const port = 8080;
     const app = express();
-    app.use(express.static('static'));
+    app.use(express.static('../static'));
     app.listen(port, () => { console.log(`Example app listening on port ${port}`) });
     
     
