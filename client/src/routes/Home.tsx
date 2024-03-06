6766663869209af1895c012b1c261a600ab33634
mdb978 <mdb978@student.bham.ac.uk>
2024-03-06 01:09:59 +0000

    layout improvements

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -12,12 +12,14 @@ export default function Homepage() {
 				}
 			</header>
 			<div
-				className='flex flex-col items-center text-darkAccentColor py-8'
+				className='flex flex-col items-center text-darkAccentColor py-8 px-2'
 				style={{ fontFamily: 'argue' }}>
-				<h2 className='text-3xl'>Welcome to the wedding of</h2>
-				<h1 className='text-6xl'>Adele Georgia Butcher</h1>
-				<h3 className='text-xl py-2'>and</h3>
-				<h1 className='text-6xl'>Matthew David Byde</h1>
+				<h2 className='text-3xl text-center'>
+					You're invited to the wedding of
+				</h2>
+				<h1 className='text-6xl text-center'>Adele Georgia Butcher</h1>
+				<h3 className='text-xl py-2 text-center'>and</h3>
+				<h1 className='text-6xl text-center'>Matthew David Byde</h1>
 			</div>
 		</PageWrapper>
 	);
