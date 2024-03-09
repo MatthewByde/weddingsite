f96b3cdb32d47ccf4158ec056790d1ab9c8ad8c4
mdb978 <mdb978@student.bham.ac.uk>
2024-03-09 23:12:32 +0000

    fix divider, add timer, make home header font size responsive

--- client/src/PageWrapper.tsx
+++ client/src/PageWrapper.tsx
@@ -2,17 +2,41 @@ import { Button, Sidebar } from 'flowbite-react';
 import React from 'react';
 import * as Icons from 'react-icons/hi2';
 import { NavCollapsedContext } from './App';
-import bg from './assets/bg.jpg'; //https://www.freepik.com/free-photo/concrete-wall-with-flowers_3164817.htm#query=wedding%20background&position=0&from_view=keyword&track=ais&uuid=aca60d95-755a-4828-88fa-7ebe1118f085 TODO ATTRIBUTE
-import Divider from './lib/Divider';
+import bg from './assets/bg.jpg'; //TODO https://www.freepik.com/free-photo/concrete-wall-with-flowers_3164817.htm#query=wedding%20background&position=0&from_view=keyword&track=ais&uuid=aca60d95-755a-4828-88fa-7ebe1118f085 ATTRIBUTE
 import { Link } from 'react-router-dom';
+import { use100vh } from 'react-div-100vh';
+import Divider from './lib/Divider';
+import dayjs from 'dayjs';
+const HEADER_SIZE = '80px';
+
+const WEDDING_DATE = dayjs('2024-11-09T12:15:00Z');
 
 export default function PageWrapper({
 	children,
 }: {
 	children?: React.ReactNode;
 }) {
+	const [timer, setTimer] = React.useState<string>('');
+
+	setInterval(() => {
+		const secondDiff = WEDDING_DATE.diff(dayjs(), 'seconds');
+		const minuteDiff = Math.floor(secondDiff / 60);
+		const hourDiff = Math.floor(minuteDiff / 60);
+		const days = Math.floor(hourDiff / 24);
+		const hours = dayjs().hour(hourDiff).hour();
+		const minutes = dayjs().minute(minuteDiff).minute();
+		const seconds = dayjs().second(secondDiff).second();
+		setTimer(
+			`${days} days, ${String(hours).padStart(2, '0')}:${String(
+				minutes
+			).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`
+		);
+	}, 1000);
+
 	// const { pathname } = useLocation();
 	const ref = React.useRef<HTMLDivElement>(null);
+	const fullvh = use100vh();
+	//TODO see what this does
 	// React.useEffect(() => {
 	// 	ref.current?.scrollTo({ behavior: 'smooth', top: 0, left: 0 });
 	// }, [pathname]);
@@ -22,7 +46,9 @@ export default function PageWrapper({
 		<div
 			className='h-full flex flex-col'
 			ref={ref}>
-			<header className='w-full h-20 bg-secondaryColor flex sticky top-0 z-50'>
+			<header
+				className='w-full bg-secondaryColor flex sticky top-0 z-50 justify-between'
+				style={{ height: HEADER_SIZE }}>
 				<Link
 					to={'/'}
 					style={{ fontFamily: 'argue' }}
@@ -31,25 +57,35 @@ export default function PageWrapper({
 					<span className='text-sm'>and</span>
 					<span className='text-lg'>Matthew</span>
 				</Link>
+				<span className='rounded-lg bg-white border-solid border-2 border-darkAccentColor h-fit self-center justify-self-end p-2 text-textColor mr-2'>
+					{timer}
+				</span>
 			</header>
 			<div className='flex'>
 				<div
-					className='sticky top-20 self-stretch'
-					style={{ height: 'calc(100vh - 5rem)' }}>
+					className='sticky self-stretch'
+					style={{
+						height: `calc(${
+							fullvh ? `${fullvh}px` : '100vh'
+						} - ${HEADER_SIZE})`,
+						top: HEADER_SIZE,
+					}}>
 					<NavBar
 						collapsed={navCollapsed}
 						setCollapsed={setNavCollapsed}
 					/>
-					<Divider
-						orientation='vertical'
-						className='relative w-[2px]'></Divider>
 				</div>
+				<Divider orientation='vertical' />
 				<div
 					className='h-full bg-fit w-full'
 					style={{ backgroundImage: `url(${bg})` }}>
 					<main
 						className='w-full pt-2 max-h-fit'
-						style={{ minHeight: 'calc(100vh - 5rem)' }}>
+						style={{
+							minHeight: `calc(${
+								fullvh ? `${fullvh}px` : '100vh'
+							} - ${HEADER_SIZE})`,
+						}}>
 						{children}
 					</main>
 				</div>
--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -15,10 +15,16 @@ export default function Homepage() {
 				className='flex flex-col items-center text-darkAccentColor py-8 px-2 [&_p]:text-center'
 				style={{ fontFamily: 'argue' }}>
 				<h1>
-					<p className='text-3xl'>You're invited to the wedding of</p>
-					<p className='text-6xl'>Adele Georgia Butcher</p>
-					<p className='text-xl py-2'>and</p>
-					<p className='text-6xl'>Matthew David Byde</p>
+					<p style={{ fontSize: 'min(max(1.875vw, 1.25rem), 1.875rem)' }}>
+						You're invited to the wedding of
+					</p>
+					<p style={{ fontSize: 'min(max(3.75vw, 2rem), 3.75rem)' }}>
+						Adele Georgia Butcher
+					</p>
+					<p style={{ fontSize: 'min(max(1.25vw, 1rem), 1.5rem)' }}>and</p>
+					<p style={{ fontSize: 'min(max(3.75vw, 2rem), 3.75rem)' }}>
+						Matthew David Byde
+					</p>
 				</h1>
 			</div>
 		</PageWrapper>
